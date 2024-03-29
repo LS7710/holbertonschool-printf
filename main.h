da597539c7ee2d8af5dba04b2ed212d537233ca4
Xavier308 <xavvis308@gmail.com>
2024-03-29 13:08:29 -0400

    Updated prototypes for more functionalities

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-void handle_format(char spec, va_list args, int *count);
+void process_specifier(char spec, va_list args, int *count);
+void print_integer(int n, int *count);
 
 #endif /* MAIN_H */
-
