39813f5596f6d5dd8c2c914ffbb3bdfd5f7b455f
luissoto7710 <luissoto7710@gmail.com>
2024-03-30 21:23:09 -0400

    test 2

--- main.h
+++ main.h
@@ -6,5 +6,7 @@
 int _printf(const char *format, ...);
 void process_specifier(char spec, va_list args, int *count);
 void print_integer(int n, int *count);
+void print_char(char c, int *count);
+void print_string(char *s, int *count);
 
 #endif /* MAIN_H */
