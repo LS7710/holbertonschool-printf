c6fefb09983d1a6ca76dbacb39039d405eda588a
Xavier308 <xavvis308@gmail.com>
2024-03-30 20:15:12 -0400

    file updated

--- print_integer.c
+++ print_integer.c
@@ -1,14 +1,25 @@
 #include "main.h"
 #include <unistd.h>
+#include <limits.h> /* Ensure this is included to use INT_MIN */
 
 /**
  * print_integer - Prints an integer
  * @n: The integer to print
  * @count: Pointer to the counter of printed characters
+ *
+ * Description: This function is designed to handle printing
+ * of integers, including the handling of INT_MIN.
  */
 void print_integer(int n, int *count)
 {
 	char num;
+
+	if (n == INT_MIN)
+	{
+		write(1, "-2147483648", 11); /* INT_MIN as a string */
+		*count += 11; /* Update the count for the length of INT_MIN */
+		return;
+	}
 
 	if (n < 0)
 	{
@@ -25,7 +36,5 @@ void print_integer(int n, int *count)
 	num = n % 10 + '0'; /* Convert digit to ASCII */
 
 	write(1, &num, 1);
-
 	(*count)++;
 }
-
